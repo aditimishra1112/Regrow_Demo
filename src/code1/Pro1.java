7d41753b3a53c58ea5738cf9b38494af49caff49
SweetyXaxa26 <sweetyxaxa28@gmail.com>
2024-10-05 17:49:56 +0530

    new changes

--- src/code1/Pro1.java
+++ src/code1/Pro1.java
@@ -5,7 +5,7 @@ public class Pro1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   
-		System.out.println("RGT program demo ");
+		System.out.println("Today is Saterday  ");
 	}
 
 }
